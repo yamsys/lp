1c72255b35934fa57763273490cfdfe71f076a95
yryugo <yryugo@yamsys.com>
2018-01-19 15:19:42 +0900

    トップページを作成

--- wp-content/themes/lightning_child/front-page.php
+++ wp-content/themes/lightning_child/front-page.php
@@ -1,91 +1,84 @@
 <?php get_header(); ?>
 
-<?php get_template_part('module_slide'); ?>
-
-
 <div class="section siteContent">
     <div class="container">
         <div class="row">
-
-            <?php
-            global $lightning_theme_options;
-            $lightning_theme_options = get_option('lightning_theme_options');
-            if (
-                isset($lightning_theme_options['top_sidebar_hidden']) &&
-                $lightning_theme_options['top_sidebar_hidden'] ){
-                $main_col = 'col-md-12';
-            } else {
-                $main_col = 'col-md-8';
-            } ?>
-
-            <div class="<?php echo $main_col; ?> mainSection">
-
-            <?php do_action('lightning_home_content_top_widget_area_before'); ?>
-
-            <?php if ( is_active_sidebar( 'home-content-top-widget-area' ) ) : ?>
-                <?php dynamic_sidebar( 'home-content-top-widget-area' ); ?>
-            <?php endif; ?>
-
-            <?php do_action('lightning_home_content_top_widget_area_after'); ?>
-
-            <?php if ( apply_filters( 'is_lightning_home_content_display', true ) ) : ?>
-
-            <?php if ( have_posts()) : ?>
-
-                <?php if ( 'page' == get_option('show_on_front') ) : ?>
-
-                    <?php while ( have_posts() ) : the_post();?>
-
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <div class="entry-body">
-                            <?php the_content(); ?>
-                        </div>
-                        <?php wp_link_pages( array( 'before' => '<div class="page-link">' . 'Pages:', 'after' => '</div>' ) ); ?>
-                         </article><!-- [ /#post-<?php the_ID(); ?> ] -->
-
-                    <?php endwhile;?>
-
-                <?php else : ?>
-
-                    <div class="postList">
-
-                        <?php while ( have_posts() ) : the_post();?>
-
-                            <?php get_template_part('module_loop_post'); ?>
-
-                        <?php endwhile;?>
-
-                        <?php the_posts_pagination(array (
-                                'mid_size'  => 1,
-                                'prev_text' => '&laquo;',
-                                'next_text' => '&raquo;',
-                                'type'      => 'list',
-                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __ ( 'Page', 'lightning' ) . ' </span>'
-                            ) ); ?>
-
-                    </div><!-- [ /.postList ] -->
-
-                <?php endif; // if ( 'page' == get_option('show_on_front') ) : ?>
-
-            <?php else: ?>
-
-                <div class="well"><p><?php _e('No posts.','lightning');?></p></div>
-
-            <?php endif; // have_post() ?>
-
-            <?php endif; // if ( apply_filters( 'is_lightning_home_top_posts_display', true ) ) : ?>
-
-            </div><!-- [ /.mainSection ] -->
-
-            <?php if ( !isset($lightning_theme_options['top_sidebar_hidden']) || !$lightning_theme_options['top_sidebar_hidden'] ) :?>
-
-                <div class="col-md-3 col-md-offset-1 subSection sideSection">
-                    <?php get_sidebar(); ?>
-                </div><!-- [ /.subSection ] -->
-
-            <?php endif; ?>
-
-        </div><!-- [ /.row ] -->
+          <div class="col-xs-4">
+            <div class="yamsys_lp_news">
+              NEWS
+            </div>
+          </div>
+          <div class="col-xs-8">
+              <div class="row">
+                <?php
+                  $args = array(
+                    'posts_per_page' => 3, // 表示件数の指定
+                    'category' => 3
+                  );
+                  $posts = get_posts( $args );
+                  foreach ( $posts as $post ): // ループの開始
+                    setup_postdata( $post ); // 記事データの取得
+                ?>
+                <a href="<?php the_permalink(); ?>">
+                <div class="col-xs-8">
+                  <div class="yamsys_lp_news_date">
+                    <?php the_date(); ?>
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="yamsys_lp_news_title">
+                    <?php the_title(); ?>
+                  </div>
+                </div>
+                </a>
+                <hr class="yamsys_lp_news_hr">
+                <?php
+                  endforeach; // ループの終了
+                  wp_reset_postdata(); // 直前のクエリを復元する
+                ?>
+              </div>
+          </div>
+        </div>
+        <hr class="yamsys_lp_hr">
+        <div class="row">
+          <div class="col-xs-4">
+            <div class="yamsys_lp_news">
+              PRESS<br />
+              RELEASE
+            </div>
+          </div>
+          <div class="col-xs-8">
+              <div class="row">
+                <?php
+                  $args = array(
+                    'posts_per_page' => 3, // 表示件数の指定
+                    'category' => 4
+                  );
+                  $posts = get_posts( $args );
+                  foreach ( $posts as $post ): // ループの開始
+                    setup_postdata( $post ); // 記事データの取得
+                ?>
+                <a href="<?php the_permalink(); ?>">
+                <div class="col-xs-8">
+                  <div class="yamsys_lp_news_date">
+                    <?php the_date(); ?>
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="yamsys_lp_news_title">
+                    <?php the_title(); ?>
+                  </div>
+                </div>
+                </a>
+                <hr class="yamsys_lp_news_hr">
+                <?php
+                  endforeach; // ループの終了
+                  wp_reset_postdata(); // 直前のクエリを復元する
+                ?>
+              </div>
+          </div>
+        </div>
+        <hr class="yamsys_lp_hr">
     </div><!-- [ /.container ] -->
 </div><!-- [ /.siteContent ] -->
  <?php get_footer(); ?>

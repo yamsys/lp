bf25b358407ffabed19340c445e772b4e7bc1370
yryugo <yryugo@yamsys.com>
2018-01-21 17:56:40 +0900

    VISION追加

---
+++ wp-content/themes/lightning_child/vision.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Template Name: Vision
+*/
+
+?>
+<?php get_header(); ?>
+
+<?php get_template_part('module_pageTit'); ?>
+<?php get_template_part('module_panList'); ?>
+
+<div class="section siteContent">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h1 class="yamsys_lp_service_h1">VISION</h1>
+        <p>
+          常に人々の立場に寄り添い、世の中すべての人々の生活を豊かにしていきます。
+        </p>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h1 class="yamsys_lp_service_h1">MISSION</h1>
+        <p>
+          ITのもたらす可能性は無限大です。その無限大のITを活用して、IT強者・弱者問わず、全人のために全人の豊かさを日々高めるための活動して参ります。
+        </p>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>

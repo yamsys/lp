c4020f730df63eb6a831ca89482c83432241e792
yryugo <yryugo@yamsys.com>
2018-01-21 17:34:50 +0900

    SERVICEを追加

---
+++ wp-content/themes/lightning_child/service.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+Template Name: サービス
+*/
+
+?>
+<?php get_header(); ?>
+
+<?php get_template_part('module_pageTit'); ?>
+<?php get_template_part('module_panList'); ?>
+
+<div class="section siteContent">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h1 class="yamsys_lp_service_h1">事業内容</h1>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    <p>
+      ITで人々の生活を豊かにするために・・・・
+    </p>
+    <br />
+    <p>
+      ITの分野で人々の生活を豊かにすることをミッションとして、様々な事業を展開しております。
+    </p>
+    <h1 class="yamsys_lp_service_title">01.システム事業</h1>
+    <p>
+      各種案件を受注します。
+    </p>
+    <p>
+      経験豊富なチームを案件ごとに編成をし、WEBアプリやHP、システム保守・運用を実施します。まずは、お見積もりください。
+    </p>
+    <div class="row">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4">
+        <a href="http://system.yamsys.com/lp_service/?date=<?php echo date('Ymd'); ?>" target="_blank" class="btn btn-danger">
+          詳細はこちら
+        </a>
+      </div>
+    </div>
+    <h1 class="yamsys_lp_service_title">02.オンライン事業</h1>
+    <p>
+      弊社で勤務する経験豊富なエンジニアやデザイナーが、皆様の夢へと後押しします。
+    </p>
+    <p>
+      学習したい内容についてカリキュラムを組むことができます。
+    </p>
+    <p>
+      カリキュラムに基づいて、カリキュラムごとのエンジニアやデザイナーからのマンツーマンレッスンを地方にいても受講することが可能です。
+    </p>
+    <div class="row">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4">
+        <a href="http://school.yamsys.com/lp_service/?date=<?php echo date('Ymd'); ?>" target="_blank" class="btn btn-danger">
+          詳細はこちら
+        </a>
+      </div>
+    </div>
+    <h1 class="yamsys_lp_service_title">03.メディア事業</h1>
+    <p>
+      IT分野にかかわらず、様々な情報を日々配信中です。
+    </p>
+    <p>
+      各事業のクポーンを配信することもあるので、お見逃しなく。
+    </p>
+    <div class="row">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4">
+        <a href="http://blog.yamsys.com/lp_service/?date=<?php echo date('Ymd'); ?>" target="_blank" class="btn btn-danger">
+          詳細はこちら
+        </a>
+      </div>
+    </div>
+    <h1 class="yamsys_lp_service_title">04.教育事業</h1>
+    <p>
+      ただいま、事業準備中です。
+    </p>
+    <div class="row">
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4">
+        <a href="" target="_blank" class="btn btn-danger">
+          詳細はこちら
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>

8ad1343c4dda5c13dd9d5c34ed093aa50f156578
yryugo <yryugo@sejuku.net>
2018-01-22 18:07:59 +0900

    recruitページを追加

---
+++ wp-content/themes/lightning_child/recruit.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+Template Name: 採用
+*/
+
+?>
+<?php get_header(); ?>
+
+<?php get_template_part('module_pageTit'); ?>
+<?php get_template_part('module_panList'); ?>
+
+<div class="section siteContent">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h1 class="yamsys_lp_service_h1">採用情報</h1>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    <p>
+      弊社では、完全リモート勤務を推奨しております。<br />
+      好きな時間に好きな場所で勤務していただけます。
+    </p>
+    <p>
+      結果にコミットしたらコミットしただけ、評価につながります。
+    </p>
+    <h1 class="yamsys_lp_service_title">01.募集ポジション</h1>
+    <div class="table-responsive">
+      <table class="table table-striped table-bordered table-hover">
+        <tr>
+          <th>部署</th>
+          <th>ポジション</th>
+          <th>リンク</th>
+        </tr>
+        <tr>
+          <td>総務部</td>
+          <td>-</td>
+          <td><a href="affairs_department" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>システム部</td>
+          <td>-</td>
+          <td><a href="system_department" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>営業部</td>
+          <td>-</td>
+          <td><a href="sales_department" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>マーケティング部</td>
+          <td>-</td>
+          <td><a href="marketing_department" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>システム事業部</td>
+          <td>-</td>
+          <td><a href="" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>オンライン事業部</td>
+          <td>-</td>
+          <td><a href="" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>メディア事業部</td>
+          <td>-</td>
+          <td><a href="" class="btn btn-danger">募集中</a></td>
+        </tr>
+        <tr>
+          <td>教育事業部</td>
+          <td>-</td>
+          <td><a href="" class="btn btn-danger">募集中</a></td>
+        </tr>
+      </table>
+    </div>
+    <h1 class="yamsys_lp_service_title">02.求める人物像</h1>
+    <ul>
+      <li>独立志向</li>
+      <li>起業志向</li>
+      <li>向上心</li>
+      <li>素直さ</li>
+      <li>誠実さ</li>
+      <li>自ら考え、自発的に行動する人</li>
+      <li>プロフェッショナル思考</li>
+    </ul>
+    <h1 class="yamsys_lp_service_title">03.働く環境</h1>
+    <p>
+      ▼自由な働き方
+    </p>
+    <ul>
+      <li>裁量労働</li>
+      <li>リモートワーク</li>
+      <li>フレックス制度</li>
+      <li>ライフスタイルに合わせた自由なコミット方法</li>
+    </ul>
+    <p>
+      ▼制度
+    </p>
+    <ul>
+      <li>1on1ミーティング</li>
+      <li>マンスリーミーティング</li>
+      <li>定例ミーティング</li>
+      <li>役職ミーティング</li>
+      <li>昇給査定年4回</li>
+      <li>売上貢献ボーナス（営業）</li>
+    </ul>
+    <p>
+      ▼福利厚生
+    </p>
+    <ul>
+      <li>各種社会保険完備（社員）</li>
+      <li>家賃補助（指定エリア）</li>
+      <li>引っ越し手当</li>
+      <li>交通費補助</li>
+      <li>扶養手当</li>
+      <li>カフェ手当</li>
+      <li>健康増進補助</li>
+      <li>研修書籍補助</li>
+      <li>部署飲み会補助</li>
+      <li>社員旅行</li>
+    </ul>
+  </div>
+</div>
+
+<?php get_footer(); ?>

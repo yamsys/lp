4a0e6eea1d8a53a2934e7a397c3d3c7c06f5a7ef
yryugo <yryugo@sejuku.net>
2018-01-22 09:49:30 +0900

    memberページを追加

---
+++ wp-content/themes/lightning_child/member.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Template Name: Member
+*/
+
+?>
+<?php get_header(); ?>
+
+<?php get_template_part('module_pageTit'); ?>
+<?php get_template_part('module_panList'); ?>
+
+<div class="section siteContent">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <h1 class="yamsys_lp_service_h1">MEMBERS</h1>
+        <p>
+          このページは現在、準備中です。
+        </p>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
